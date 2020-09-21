400e088e08074009c96333936dce14d8dc802f8c
Shawn Kearney <shawnpk29@me.com>
2020-09-21 06:53:07 -0600

    Generate migration to allow user to unsubscribe from emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_182426) do
+ActiveRecord::Schema.define(version: 2020_09_21_125248) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_182426) do
     t.boolean "notify_when_added_to_project", default: true
     t.boolean "notify_when_task_created", default: true
     t.boolean "notify_when_task_completed", default: true
+    t.string "unsubscribe_hash"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,6 +14,7 @@
 #  remember_created_at          :datetime
 #  reset_password_sent_at       :datetime
 #  reset_password_token         :string
+#  unsubscribe_hash             :string
 #  created_at                   :datetime         not null
 #  updated_at                   :datetime         not null
 #
